04ab2dca4a551550814beb4fdb11abc48aef9e03
konda_yuto <konda.yuto@alhinc.jp>
2026-05-11 15:01:02 +0900

    Practical question 1

--- src/chapter6/dao/UserDao.java
+++ src/chapter6/dao/UserDao.java
@@ -11,6 +11,8 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang.StringUtils;
+
 import chapter6.beans.User;
 import chapter6.exception.NoRowsUpdatedRuntimeException;
 import chapter6.exception.SQLRuntimeException;
@@ -194,7 +196,7 @@ public class UserDao {
 			sql.append("    account = ?, ");
 			sql.append("    name = ?, ");
 			sql.append("    email = ?, ");
-			if (user.getPassword() != "") {
+			if (StringUtils.isBlank(user.getPassword()))  {
 				sql.append("    password = ?, ");
 			}
 			sql.append("    description = ?, ");
@@ -206,7 +208,7 @@ public class UserDao {
 			ps.setString(1, user.getAccount());
 			ps.setString(2, user.getName());
 			ps.setString(3, user.getEmail());
-			if (user.getPassword() != "") {
+			if (StringUtils.isBlank(user.getPassword()))  {
 				ps.setString(4, user.getPassword());
 				ps.setString(5, user.getDescription());
 				ps.setInt(6, user.getId());
--- src/chapter6/service/UserService.java
+++ src/chapter6/service/UserService.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang.StringUtils;
+
 import chapter6.beans.User;
 import chapter6.dao.UserDao;
 import chapter6.logging.InitApplication;
@@ -131,7 +133,7 @@ public class UserService {
 		Connection connection = null;
 		try {
 			// パスワード暗号化
-			if (user.getPassword() != "") {
+			if (StringUtils.isBlank(user.getPassword())) {
 				String encPassword = CipherUtil.encrypt(user.getPassword());
 				user.setPassword(encPassword);
 			}
