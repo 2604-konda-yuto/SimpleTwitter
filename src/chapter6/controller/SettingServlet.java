de1e46efd54513cd1ab177ec930daf06d96f39e9
konda_yuto <konda.yuto@alhinc.jp>
2026-05-14 16:17:17 +0900

    Practical problem 3

--- src/chapter6/controller/SettingServlet.java
+++ src/chapter6/controller/SettingServlet.java
@@ -116,7 +116,7 @@ public class SettingServlet extends HttpServlet {
 		String account = user.getAccount();
 		String email = user.getEmail();
 
-		User Account = new UserService().select(account);
+		User duplication = new UserService().select(account);
 		if (!StringUtils.isEmpty(name) && (20 < name.length())) {
 			errorMessages.add("名前は20文字以下で入力してください");
 		}
@@ -124,7 +124,7 @@ public class SettingServlet extends HttpServlet {
 			errorMessages.add("アカウント名を入力してください");
 		} else if (20 < account.length()) {
 			errorMessages.add("アカウント名は20文字以下で入力してください");
-		} else if (Account != null && Account.getId() != user.getId()) {
+		} else if (duplication != null && duplication.getId() != user.getId()) {
 			errorMessages.add("すでに存在するアカウントです");
 		}
 		if (StringUtils.isEmpty(email)) {
--- src/chapter6/controller/SignUpServlet.java
+++ src/chapter6/controller/SignUpServlet.java
@@ -96,7 +96,7 @@ public class SignUpServlet extends HttpServlet {
 		String password = user.getPassword();
 		String email = user.getEmail();
 
-		User Account = new UserService().select(account);
+		User duplication = new UserService().select(account);
 
 		if (!StringUtils.isEmpty(name) && (20 < name.length())) {
 			errorMessages.add("名前は20文字以下で入力してください");
@@ -106,7 +106,7 @@ public class SignUpServlet extends HttpServlet {
 			errorMessages.add("アカウント名を入力してください");
 		} else if (20 < account.length()) {
 			errorMessages.add("アカウント名は20文字以下で入力してください");
-		} else if (Account != null) {
+		} else if (duplication != null) {
 			errorMessages.add("すでに存在するアカウントです");
 		}
 
