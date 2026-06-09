9c3610bd77bb6239a79d948e9852c84625a2e05d
konda_yuto <konda.yuto@alhinc.jp>
2026-06-09 16:07:41 +0900

    addcomment UserCommentDao

--- src/chapter6/dao/UserCommentDao.java
+++ src/chapter6/dao/UserCommentDao.java
@@ -43,17 +43,31 @@ public class UserCommentDao {
 		try {
 			StringBuilder sql = new StringBuilder();
 			sql.append("SELECT ");
+			//参照
 			sql.append("    comments.id as id, ");
+			//返信IDをas句によりidに別名変更
 			sql.append("    comments.text as text, ");
+			//返信をas句によりtextに別名変更
 			sql.append("    comments.user_id as user_id, ");
+			//ユーザIDをas句により	user_idに別名変更
 			sql.append("    comments.message_id as message_id, ");
+			//つぶやきIDをas句によりmessage_idに別名変更
 			sql.append("    users.account as account, ");
+			//アカウントをas句によりaccountに別名変更
 			sql.append("    users.name as name, ");
+			//名前をas句によりnameに別名変更
 			sql.append("    comments.created_date as created_date ");
+			//作成日をas句によりcreated_dateに別名変更
 			sql.append("FROM comments ");
+			//【comments】データベースから情報取得
 			sql.append("INNER JOIN users ");
+			//【users】データベースと【comments】データベースの
+			// usersデータを抽出して結合
 			sql.append("ON comments.user_id = users.id ");
+			//comments.user_idとusers.idが一致した際に結合
 			sql.append("ORDER BY created_date ASC limit " + num);
+			//つぶやき作成日のデータのソート順を昇順にし、
+			//取得するデータの件数を1000件に制限する
 
 			ps = connection.prepareStatement(sql.toString());
 
@@ -79,16 +93,24 @@ public class UserCommentDao {
 				}.getClass().getEnclosingMethod().getName());
 
 		List<UserComment> comments = new ArrayList<UserComment>();
+		//beansから取得したArrayList<UserComment>をList<UserComment>型のcommentsに格納し作成
 		try {
 			while (rs.next()) {
 				UserComment comment = new UserComment();
 				comment.setId(rs.getInt("id"));
+				//beans.UserCommentから取得したidをcommentに格納
 				comment.setText(rs.getString("text"));
+				//beans.UserCommentから取得したtextをcommentに格納
 				comment.setUserId(rs.getInt("user_id"));
+				//beans.UserCommentから取得したuser_idをcommentに格納
 				comment.setAccount(rs.getString("account"));
+				//beans.UserCommentから取得したaccountをcommentに格納
 				comment.setName(rs.getString("name"));
+				//beans.UserCommentから取得したnameをcommentに格納
 				comment.setMessageId(rs.getInt("message_id"));
+				//beans.UserCommentから取得したmessager_idをcommentに格納
 				comment.setCreatedDate(rs.getTimestamp("created_date"));
+				//beans.UserCommentから取得したcreated_dateをcommentに格納
 
 				comments.add(comment);
 			}
